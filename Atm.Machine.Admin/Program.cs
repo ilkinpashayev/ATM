d0d0f23650a8f9ccc44c9f726a8ef9397ad6ff57
Ilkin <pashayev.ilkin@gmail.com>
2019-01-10 18:47:50 +0400

    ATM TASK

--- Atm.Machine.Admin/Program.cs
+++ Atm.Machine.Admin/Program.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Atm.Machine.Models;
-using Atm.Machine.Services.Exceptions;
-using Atm.Machine.Services.Interfaces;
-using Atm.Machine.App;
+﻿using Atm.Machine.App;
 using Atm.Machine.Services.Menu;
 
-namespace Atm.Machine.Backend
+
+namespace Atm.Machine.Admin
 {
     class Program
     {
--- Atm.Machine.Admin/Properties/AssemblyInfo.cs
+++ Atm.Machine.Admin/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Atm.Machine.Backend")]
+[assembly: AssemblyTitle("Atm.Machine.Admin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Atm.Machine.Backend")]
+[assembly: AssemblyProduct("Atm.Machine.Admin")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("24a776a7-dfeb-4ca6-ba9b-c2be5b96ce00")]
+[assembly: Guid("26eac696-8738-4358-a1e2-4b223733e43f")]
 
 // Version information for an assembly consists of the following four values:
 //
